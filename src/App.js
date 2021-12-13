e57575a77d66df96b6d1bcb5dd81a9756df8812b
Alkindy <mohammedalkindy123@gmail.com>
2021-12-13 22:15:30 +0300

    Updating

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const particlesOptions = {
 const initialState ={
   input: '',
   imageUrl: '',
-  box: {},
+  boxes: [],
   route: 'signin',
   isSignedIn: false,
   user: {
@@ -55,21 +55,23 @@ class App extends Component {
   }
 
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFaceLocations = (data) => {
+    return data.outputs[0].data.regions.map(face => {
+    const clarifaiFace = face.region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    });
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBox = (boxes) => {
+    this.setState({boxes: boxes});
   }
 
   onInputChange = (event) => {
@@ -78,7 +80,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://fast-atoll-87401.herokuapp.com/imageurl', {
+    fetch('http://localhost:3000/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -88,7 +90,7 @@ class App extends Component {
     .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('https://fast-atoll-87401.herokuapp.com/image', {
+          fetch('http://localhost:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -101,7 +103,7 @@ class App extends Component {
             })
 
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox(this.calculateFaceLocations(response))
       })
       .catch(err => console.log(err));
 }
@@ -115,7 +117,7 @@ class App extends Component {
     }
 
     render() {
-      const { isSignedIn, imageUrl, route, box } = this.state;
+      const { isSignedIn, imageUrl, route, boxes } = this.state;
       return (
         <div className="App">
            <Particles className='particles'
@@ -130,7 +132,7 @@ class App extends Component {
                   onInputChange={this.onInputChange}
                   onButtonSubmit={this.onButtonSubmit}
                 />
-                <FaceRecognition box={box} imageUrl={imageUrl} />
+                <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
               </div>
             : (
                route === 'signin'
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://fast-atoll-87401.herokuapp.com/register', {
+    fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
